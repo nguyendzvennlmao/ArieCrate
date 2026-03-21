765b9529e8add228c677856a7b531187df1b3161
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-21 19:50:27 +0700

    Update ArisCrate.java

--- src/main/java/me/aris/crates/ArisCrate.java
+++ src/main/java/me/aris/crates/ArisCrate.java
@@ -3,32 +3,40 @@ package me.aris.crates;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import java.io.File;
 
 public class ArisCrate extends JavaPlugin {
     private CrateManager crateManager;
-    private File crateFile, msgFile, keyFile;
-    private FileConfiguration crateConfig, msgConfig, keyConfig;
+    private File commonFile, keyFile, msgFile;
+    private FileConfiguration commonConfig, keyConfig, msgConfig;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         loadFiles();
         crateManager = new CrateManager(this);
+        
         CrateCommand cmd = new CrateCommand(this);
         getCommand("ariscrate").setExecutor(cmd);
         getCommand("ariscrate").setTabCompleter(cmd);
+        
         getServer().getPluginManager().registerEvents(new CrateListener(this), this);
+        getLogger().info("ArisCrate has been enabled!");
     }
 
     public void loadFiles() {
         if (!getDataFolder().exists()) getDataFolder().mkdirs();
-        crateFile = new File(getDataFolder(), "crate.yml");
-        if (!crateFile.exists()) saveResource("crate.yml", false);
-        crateConfig = YamlConfiguration.loadConfiguration(crateFile);
+        
+        commonFile = new File(getDataFolder(), "common.yml");
+        if (!commonFile.exists()) saveResource("common.yml", false);
+        commonConfig = YamlConfiguration.loadConfiguration(commonFile);
+
         msgFile = new File(getDataFolder(), "message.yml");
         if (!msgFile.exists()) saveResource("message.yml", false);
         msgConfig = YamlConfiguration.loadConfiguration(msgFile);
+
         keyFile = new File(getDataFolder(), "playerkey.yml");
         if (!keyFile.exists()) try { keyFile.createNewFile(); } catch (Exception e) {}
         keyConfig = YamlConfiguration.loadConfiguration(keyFile);
@@ -36,14 +44,18 @@ public class ArisCrate extends JavaPlugin {
 
     public void sendMsg(org.bukkit.entity.Player p, String path, String... replace) {
         if (!msgConfig.contains("messages." + path)) return;
-        String msg = crateManager.translateHex(msgConfig.getString("messages." + path + ".text", ""));
+        String msg = crateManager.translateHex(msgConfig.getString("messages." + path + ".text"));
         for (int i = 0; i < replace.length; i += 2) msg = msg.replace(replace[i], replace[i+1]);
-        p.sendMessage(msg);
+        
+        if (msgConfig.getBoolean("messages." + path + ".chat", true)) p.sendMessage(msg);
+        if (msgConfig.getBoolean("messages." + path + ".actionbar", false)) {
+            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg));
+        }
     }
 
-    public FileConfiguration getCrateConfig() { return crateConfig; }
+    public FileConfiguration getCommonConfig() { return commonConfig; }
+    public void saveCommonConfig() { try { commonConfig.save(commonFile); } catch (Exception e) {} }
     public FileConfiguration getKeyConfig() { return keyConfig; }
-    public void saveCrateConfig() { try { crateConfig.save(crateFile); } catch (Exception e) {} }
     public void saveKeyConfig() { try { keyConfig.save(keyFile); } catch (Exception e) {} }
     public CrateManager getCrateManager() { return crateManager; }
-}
+            }
