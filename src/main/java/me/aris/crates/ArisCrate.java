fe962ea7dd9d769ab9064e8a8788a8dd1f29d4d0
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-21 20:06:44 +0700

    Update ArisCrate.java

--- src/main/java/me/aris/crates/ArisCrate.java
+++ src/main/java/me/aris/crates/ArisCrate.java
@@ -9,29 +9,32 @@ import java.io.File;
 
 public class ArisCrate extends JavaPlugin {
     private CrateManager crateManager;
-    private File commonFile, keyFile, msgFile;
-    private FileConfiguration commonConfig, keyConfig, msgConfig;
+    private File keyFile, msgFile;
+    private FileConfiguration keyConfig, msgConfig;
+    private boolean isFolia;
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
+        try {
+            Class.forName("io.papermc.paper.threadedregionsapi.RegionScheduler");
+            isFolia = true;
+        } catch (ClassNotFoundException e) {
+            isFolia = false;
+        }
         loadFiles();
         crateManager = new CrateManager(this);
-        
         CrateCommand cmd = new CrateCommand(this);
         getCommand("ariscrate").setExecutor(cmd);
         getCommand("ariscrate").setTabCompleter(cmd);
-        
         getServer().getPluginManager().registerEvents(new CrateListener(this), this);
-        getLogger().info("ArisCrate has been enabled!");
     }
 
     public void loadFiles() {
+        saveDefaultConfig();
+        reloadConfig();
         if (!getDataFolder().exists()) getDataFolder().mkdirs();
-        
-        commonFile = new File(getDataFolder(), "common.yml");
-        if (!commonFile.exists()) saveResource("common.yml", false);
-        commonConfig = YamlConfiguration.loadConfiguration(commonFile);
+        File cratesFolder = new File(getDataFolder(), "crates");
+        if (!cratesFolder.exists()) cratesFolder.mkdirs();
 
         msgFile = new File(getDataFolder(), "message.yml");
         if (!msgFile.exists()) saveResource("message.yml", false);
@@ -46,15 +49,13 @@ public class ArisCrate extends JavaPlugin {
         if (!msgConfig.contains("messages." + path)) return;
         String msg = crateManager.translateHex(msgConfig.getString("messages." + path + ".text"));
         for (int i = 0; i < replace.length; i += 2) msg = msg.replace(replace[i], replace[i+1]);
-        
         if (msgConfig.getBoolean("messages." + path + ".chat", true)) p.sendMessage(msg);
         if (msgConfig.getBoolean("messages." + path + ".actionbar", false)) {
             p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg));
         }
     }
 
-    public FileConfiguration getCommonConfig() { return commonConfig; }
-    public void saveCommonConfig() { try { commonConfig.save(commonFile); } catch (Exception e) {} }
+    public boolean isFolia() { return isFolia; }
     public FileConfiguration getKeyConfig() { return keyConfig; }
     public void saveKeyConfig() { try { keyConfig.save(keyFile); } catch (Exception e) {} }
     public CrateManager getCrateManager() { return crateManager; }
