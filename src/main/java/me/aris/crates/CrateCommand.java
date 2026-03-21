f175d0460b82157922e4a3bac082364f3f10fcaf
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-21 20:24:54 +0700

    Update CrateCommand.java

--- src/main/java/me/aris/crates/CrateCommand.java
+++ src/main/java/me/aris/crates/CrateCommand.java
@@ -19,24 +19,32 @@ public class CrateCommand implements CommandExecutor, TabCompleter {
         if (sub.equals("reload")) {
             plugin.loadFiles();
             if (sender instanceof Player) plugin.sendMsg((Player) sender, "reload-success");
-            else sender.sendMessage("§aReloaded successfully!");
+            else sender.sendMessage("✔ Reloaded successfully!");
             return true;
         }
 
-        if (sub.equals("create") && args.length > 1 && sender instanceof Player) {
-            String name = args[1];
+        if (sender instanceof Player) {
             Player p = (Player) sender;
-            Location loc = p.getLocation();
-            var cfg = plugin.getCrateManager().getCrateConfig(name);
-            cfg.set("location.world", loc.getWorld().getName());
-            cfg.set("location.x", loc.getBlockX());
-            cfg.set("location.y", loc.getBlockY());
-            cfg.set("location.z", loc.getBlockZ());
-            try { cfg.save(new File(plugin.getDataFolder() + "/crates", name + ".yml")); } catch (Exception e) {}
-            plugin.sendMsg(p, "crate-create", "%crate%", name);
-        } else if (sub.equals("edit") && args.length > 1 && sender instanceof Player) {
-            plugin.getCrateManager().openEditMenu((Player) sender, args[1]);
-        } else if (sub.equals("givekey") && args.length > 3) {
+            if (sub.equals("create") || sub.equals("movehere")) {
+                if (args.length < 2) return true;
+                String name = args[1];
+                Location loc = p.getLocation();
+                var cfg = plugin.getCrateManager().getCrateConfig(name);
+                cfg.set("location.world", loc.getWorld().getName());
+                cfg.set("location.x", loc.getBlockX());
+                cfg.set("location.y", loc.getBlockY());
+                cfg.set("location.z", loc.getBlockZ());
+                try { cfg.save(new File(plugin.getDataFolder() + "/crates", name + ".yml")); } catch (Exception e) {}
+                plugin.sendMsg(p, "crate-create", "%crate%", name);
+                return true;
+            }
+            if (sub.equals("edit") && args.length > 1) {
+                plugin.getCrateManager().openEditMenu(p, args[1]);
+                return true;
+            }
+        }
+
+        if (sub.equals("givekey") && args.length > 3) {
             Player target = Bukkit.getPlayer(args[1]);
             if (target != null) plugin.getCrateManager().giveKey(target, args[2], Integer.parseInt(args[3]));
         }
@@ -45,7 +53,7 @@ public class CrateCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length == 1) return Arrays.asList("create", "edit", "givekey", "reload");
+        if (args.length == 1) return Arrays.asList("create", "movehere", "edit", "givekey", "reload");
         List<String> crates = new ArrayList<>();
         File folder = new File(plugin.getDataFolder(), "crates");
         if (folder.exists() && folder.listFiles() != null) {
@@ -56,4 +64,4 @@ public class CrateCommand implements CommandExecutor, TabCompleter {
         if (args.length == 3 && args[0].equalsIgnoreCase("givekey")) return crates;
         return new ArrayList<>();
     }
-                   }
+                        }
