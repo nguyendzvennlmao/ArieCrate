3cc1e2335d0af914e25aa96e59f32532fd39d220
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-22 01:28:27 +0700

    Update CrateExpansion.java

--- src/main/java/me/aris/crates/CrateExpansion.java
+++ src/main/java/me/aris/crates/CrateExpansion.java
@@ -6,14 +6,11 @@ import org.jetbrains.annotations.NotNull;
 
 public class CrateExpansion extends PlaceholderExpansion {
     private final ArisCrate plugin;
-
     public CrateExpansion(ArisCrate plugin) { this.plugin = plugin; }
-
     @Override @NotNull public String getAuthor() { return "Aris"; }
     @Override @NotNull public String getIdentifier() { return "ariscrate"; }
     @Override @NotNull public String getVersion() { return "1.0"; }
     @Override public boolean persist() { return true; }
-
     @Override
     public String onRequest(OfflinePlayer player, @NotNull String params) {
         if (player == null) return "0";
@@ -23,4 +20,4 @@ public class CrateExpansion extends PlaceholderExpansion {
         }
         return null;
     }
-}
+                                                               }
