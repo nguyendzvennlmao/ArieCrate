fc65cab120b111a68d15cb5ba245bfe8a4acdb22
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 12:55:33 +0700

    Update ArisCrate.java

--- src/main/java/me/aris/ariscrate/ArisCrate.java
+++ src/main/java/me/aris/ariscrate/ArisCrate.java
@@ -1,75 +1,94 @@
 package me.aris.ariscrate;
 
-import me.aris.ariscrate.command.CrateAdminCommand;
-import me.aris.ariscrate.crate.CrateManager;
 import me.aris.ariscrate.key.KeyManager;
-import me.aris.ariscrate.listener.CrateInteractListener;
-import me.aris.ariscrate.listener.MenuListener;
+import me.aris.ariscrate.task.KeyAllTask;
 import me.aris.ariscrate.utils.ColorUtils;
-import me.aris.ariscrate.utils.KeyExpansion;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
+import me.aris.ariscrate.listener.MenuListener;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
-public class ArisCrate extends JavaPlugin {
-    private static ArisCrate instance;
-    private CrateManager crateManager;
+public class ArisCrate extends JavaPlugin implements CommandExecutor, TabCompleter {
     private KeyManager keyManager;
-    private FileConfiguration msgConfig;
-    private FileConfiguration guiConfig;
-    private final Map<UUID, String> selectingCrate = new HashMap<>();
+    private KeyAllTask keyAllTask;
+    private Map<UUID, String> selectingCrate = new HashMap<>();
 
     @Override
     public void onEnable() {
-        instance = this;
         saveDefaultConfig();
-        loadConfigs();
-        this.crateManager = new CrateManager(this);
+        File cratesFolder = new File(getDataFolder(), "crates");
+        if (!cratesFolder.exists()) cratesFolder.mkdirs();
         this.keyManager = new KeyManager(this);
-        getCommand("ariscrate").setExecutor(new CrateAdminCommand(this));
-        getCommand("ariscrate").setTabCompleter(new CrateAdminCommand(this));
-        Bukkit.getPluginManager().registerEvents(new CrateInteractListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new MenuListener(this), this);
-        this.crateManager.load();
         this.keyManager.load();
-        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new KeyExpansion(this).register();
+        this.keyAllTask = new KeyAllTask(this);
+        getCommand("acrate").setExecutor(this);
+        getCommand("acrate").setTabCompleter(this);
+        Bukkit.getPluginManager().registerEvents(new MenuListener(this), this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length < 3) return true;
+        if (args[0].equalsIgnoreCase("givekeyall")) {
+            String type = args[1];
+            int amt = Integer.parseInt(args[2]);
+            if (args.length > 3) {
+                Player target = Bukkit.getPlayer(args[3]);
+                if (target != null) give(target, type, amt, false);
+            } else {
+                Bukkit.getOnlinePlayers().forEach(p -> give(p, type, amt, false));
+            }
+            return true;
         }
+        return true;
     }
 
-    public void loadConfigs() {
-        reloadConfig();
-        msgConfig = loadResourceFile("message.yml");
-        guiConfig = loadResourceFile("gui.yml");
-    }
-
-    private FileConfiguration loadResourceFile(String name) {
-        File file = new File(getDataFolder(), name);
-        if (!file.exists()) saveResource(name, false);
-        return YamlConfiguration.loadConfiguration(file);
-    }
-
-    public void sendNotify(Player p, String path, String... placeholders) {
-        String raw = msgConfig.getString("messages." + path);
-        if (raw == null) return;
-        for (int i = 0; i < placeholders.length; i += 2) {
-            raw = raw.replace(placeholders[i], placeholders[i + 1]);
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+        if (args.length == 1) return List.of("givekeyall");
+        if (args[0].equalsIgnoreCase("givekeyall")) {
+            if (args.length == 2) {
+                File folder = new File(getDataFolder(), "crates");
+                File[] files = folder.listFiles();
+                if (files != null) return Arrays.stream(files).map(f -> f.getName().replace(".yml", "")).collect(Collectors.toList());
+                return List.of("common");
+            }
+            if (args.length == 3) return List.of("1", "10", "64");
+            if (args.length == 4) return null;
         }
-        String msg = ColorUtils.color(getConfig().getString("settings.prefix") + raw);
-        if (getConfig().getBoolean("notification.chat")) p.sendMessage(msg);
-        if (getConfig().getBoolean("notification.actionbar")) p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(msg));
+        return Collections.emptyList();
     }
 
-    public static ArisCrate getInstance() { return instance; }
-    public CrateManager getCrateManager() { return crateManager; }
+    public void give(Player p, String type, int amt, boolean isAuto) {
+        keyManager.addKeys(p.getUniqueId(), type, amt);
+        String sub = isAuto ? getConfig().getString("keyall.subtitle") : 
+                     getMessageConfig().getString("messages.receive-give").replace("%key%", type);
+        p.sendTitle("", ColorUtils.color(sub), 10, 40, 10);
+        p.playSound(p.getLocation(), Sound.valueOf(getConfig().getString("sounds.receive-key")), 1, 1);
+        sendNotify(p, "receive-give", "%key%", type, "%amount%", String.valueOf(amt));
+    }
+
+    public void sendNotify(Player p, String path, String... ph) {
+        var msgCfg = getMessageConfig();
+        String msg = msgCfg.getString("messages." + path);
+        if (msg == null) return;
+        for (int i = 0; i < ph.length; i += 2) msg = msg.replace(ph[i], ph[i + 1]);
+        String fin = ColorUtils.color(getConfig().getString("settings.prefix") + msg);
+        if (msgCfg.getBoolean("chat")) p.sendMessage(fin);
+        if (msgCfg.getBoolean("actionbar")) p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(fin));
+    }
+
     public KeyManager getKeyManager() { return keyManager; }
-    public FileConfiguration getGuiConfig() { return guiConfig; }
-    public FileConfiguration getMsgConfig() { return msgConfig; }
+    public KeyAllTask getKeyAllTask() { return keyAllTask; }
     public Map<UUID, String> getSelectingCrate() { return selectingCrate; }
-}
+            }
