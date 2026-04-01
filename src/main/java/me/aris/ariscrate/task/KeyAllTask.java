620851692977be9d1f39769cdca729ba1d1f5cce
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 13:29:29 +0700

    Update KeyAllTask.java

--- src/main/java/me/aris/ariscrate/task/KeyAllTask.java
+++ src/main/java/me/aris/ariscrate/task/KeyAllTask.java
@@ -2,43 +2,23 @@ package me.aris.ariscrate.task;
 
 import me.aris.ariscrate.ArisCrate;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class KeyAllTask {
-    private long seconds;
-    private final long resetTime;
+public class KeyAllTask extends BukkitRunnable {
     private final ArisCrate plugin;
 
     public KeyAllTask(ArisCrate plugin) {
         this.plugin = plugin;
-        this.resetTime = parse(plugin.getConfig().getString("keyall.time"));
-        this.seconds = resetTime;
-        run();
     }
 
-    private void run() {
-        Bukkit.getGlobalRegionScheduler().runAtFixedRate(plugin, t -> {
-            if (seconds <= 0) {
-                Bukkit.getOnlinePlayers().forEach(p -> plugin.give(p, "common", 1, true));
-                seconds = resetTime;
-            }
-            seconds--;
-        }, 20L, 20L);
+    @Override
+    public void run() {
+        String keyType = "common";
+        int amount = 1;
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            plugin.getKeyManager().give(player, keyType, amount, true);
+            String sub = plugin.getConfig().getString("keyall.subtitle");
+            if (sub != null) player.sendTitle("", plugin.format(sub), 10, 40, 10);
+        });
     }
-
-    private long parse(String s) {
-        if (s == null) return 3600;
-        s = s.toLowerCase();
-        if (s.endsWith("h")) return Long.parseLong(s.replace("h", "")) * 3600;
-        if (s.endsWith("m")) return Long.parseLong(s.replace("m", "")) * 60;
-        if (s.endsWith("s")) return Long.parseLong(s.replace("s", ""));
-        return Long.parseLong(s);
-    }
-
-    public String getTime() {
-        long h = seconds / 3600;
-        long m = (seconds % 3600) / 60;
-        long s = seconds % 60;
-        if (h > 0) return String.format("%02d:%02d:%02d", h, m, s);
-        return String.format("%02d:%02d", m, s);
-    }
-                  }
+}
