1a02470e560f6966955341b788e07992f15a8beb
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-01 10:44:25 +0700

    Create ColorUtils.java

---
+++ src/main/java/me/aris/ariscrate/utils/ColorUtils.java
@@ -0,0 +1,17 @@
+package me.aris.ariscrate.utils;
+
+import net.md_5.bungee.api.ChatColor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorUtils {
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
+    public static String color(String message) {
+        Matcher matcher = HEX_PATTERN.matcher(message);
+        StringBuilder buffer = new StringBuilder();
+        while (matcher.find()) {
+            matcher.appendReplacement(buffer, ChatColor.of("#" + matcher.group(1)).toString());
+        }
+        return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
+    }
+  }
